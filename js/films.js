5a6bb6bcdcc29e9a4083109a1fad8b067f2b3b01
doigorek <yngvvar@gmail.com>
2019-11-18 23:16:27 +0300

    4.8-this

--- js/films.js
+++ js/films.js
@@ -1,45 +1,193 @@
-const ganars = ['фантастика ', 'боевик ', 'приключения ', 'фэнтези ', 'драмма ', 'комедия '];
-const films = [
-    ['10:00', 'Человек паук', [2, 3, 4] ],
-    ['12:00', 'Собачья жизнь 2', [1, 1]],
-    ['14:00', 'История игрушек 4', [2, 2, 2, 4]],
-    ['16:00', 'Люди в черном: Интернэшнл', [4]]
-];
+films = [
+    film1 = {
+        name: 'Человек паук',
+        start: '10:00',
+        genre: [0, 1],
+        image: "img/mov1.jpg",
+        hire: true,
+        new: true,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+    film2 = {
+        name: 'Собачья жизнь 2',
+        start: '12:00',
+        genre: [2, 3, 4],
+        image: "img/mov1.jpg",
+        hire: true,
+        new: true,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+    film3 = {
+        name: 'История игрушек 4',
+        start: '15:00',
+        genre: [2, 4, 5],
+        image: "img/mov2.jpg",
+        hire: true,
+        new: false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+    film4 = {
+        name: 'Люди в черном: Интернэшнл',
+        start: '19:00',
+        genre: [0, 1, 4],
+        image: "img/mov3.jpg",
+        hire: true,
+        new:false,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+    film5 = {
+        name: 'Собачья жизнь 2',
+        start: '19:00',
+        genre: [0, 1, 4],
+        image: "img/mov2.jpg",
+        hire: false,
+        new:true,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+    film6 = {
+        name: 'Люди в черном: Интернэшнл',
+        start: '19:00',
+        genre: [0, 1, 4],
+        image: "img/mov3.jpg",
+        hire: false,
+        new: true,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        fb: "https://fb.com",
+        twitter: "https://twitter.com",
+        behance: "https://www.behance.net"
+    },
+]
+
+//Справочник жанров
+const genre = [
+    'Фантастика ',   //0
+    'Боевик ',       //1
+    'Фэнтези ',      //2
+    'Драма ',        //3
+    'Комедия ',      //4
+    'Мультфильм ',   //5
+]
 
 
+// для секции
+let filmsHire = [];
+// для секции слайдер
+let filmsNew = [];
 
-const filmstTableBody = document.getElementById('films-table-body');
-for (let i = 0; i < films.length; i++) {
-    
-    let ganresIndex;
-    let currentFilmsGanars = '';
-    for(let j = 0; j < ganars.length; j++)
-    {
-        ganresIndex = films[i][2][j];
-        if(ganresIndex != undefined){
-            currentFilmsGanars += String(ganars[ganresIndex]);
-        } 
+for(let i=0; i < films.length; i++){
+    if(films[i].hire === true){
+        filmsHire.push(films[i])
     }
     
-    // let ganresIndex = films[i][2][0]
-    // let currentFilmsGanars = ganars[ganresIndex]
+    if(films[i].hire === false && films[i].new === true) {
+        filmsNew.push(films[i])
+    }
+}
 
-    filmstTableBody.innerHTML += `
-        <tr>
-            <td>${films[i][0]}</td>
-            <td><a href="https://www.kinopoisk.ru/film/838/" target="_blank">${films[i][1]}</a></td>
-            
-            <td>${currentFilmsGanars}</td>
 
+const film = {
+    getFb: function() {
+        return this.fb;
+    },
+    getTwitter: function() {
+        return this.twitter;
+    },
+    getBehance: function() {
+        return this.behance;
+    },
+    getDescription: function() {
+        return this.description;
+    },
+    getImg: function() {
+        return this.image;
+    },
+    getName: function () {
+        return this.name;  
+    },
+    getStart: function () {
+        return this.start;   
+    },
+    getGenre: function () {
+        const genreFilm = this.genre;
+        let arrGenre = [];
+        for(let i=0; i < genreFilm.length; i++){
+            arrGenre.push(genre[genreFilm[i]]);
+        }
+        let strGenre = arrGenre.join(', ');
+        return strGenre;     //return this.strGenre;   
+    }
+}
+
+
+
+for(let i = 0; i< filmsHire.length; i++) {
+    const filmName = film.getName.bind(filmsHire[i])();
+    const filmStart = film.getStart.bind(filmsHire[i])();
+    const filmGenre = film.getGenre.bind(filmsHire[i])();
+    let filmsHireHTML = document.getElementById('filmsHire')
+
+
+    let filmHTML = `<tr>
+            <td id="start_film_2">${filmStart}</td>
+            <td id="name_film_2">
+            <a href="https://www.kinopoisk.ru/film/sobachya-zhizn-2-2019-1122114/" target="_blank">
+            ${filmName}
+            </a></td>
+            <td id="ganar_film_2">${filmGenre}</td>
             <td>
                 <svg 
                     xmlns="http://www.w3.org/2000/svg"
                     xmlns:xlink="http://www.w3.org/1999/xlink"
                     width="33px" height="33px">
-                <path fill-rule="evenodd"  fill="rgb(255, 255, 255)"
+                    <path fill-rule="evenodd"  fill="rgb(255, 255, 255)"
                     d="M16.505,32.994 C7.393,32.994 0.005,25.607 0.005,16.494 C0.005,7.381 7.393,-0.006 16.505,-0.006 C25.618,-0.006 33.005,7.381 33.005,16.494 C33.005,25.607 25.618,32.994 16.505,32.994 ZM24.214,14.591 L18.356,14.631 L18.356,8.796 L13.958,8.777 L13.958,14.637 L8.083,14.637 L8.083,19.074 L13.958,19.074 L13.958,24.889 L18.336,24.889 L18.307,19.074 L24.231,19.074 L24.214,14.591 Z"/>
                 </svg>
             </td>
-        </tr>
-    `
+        </tr>`
+    let tr = document.createElement("tr");
+    tr.innerHTML = filmHTML;
+    filmsHireHTML.appendChild(tr);
 }
+
+const movieGrid = document.getElementById('movie-grid');
+const tmpMovieGrid = document.getElementById('template-movie-box');
+
+for(let i = 0; i< filmsNew.length; i++){
+    const filmBehance = film.getBehance.bind(filmsNew[i])();
+    const filmTwitter = film.getTwitter.bind(filmsNew[i])();
+    const filmFb = film.getFb.bind(filmsNew[i])();
+    const filmDescription = film.getDescription.bind(filmsNew[i])();
+    const filmName = film.getName.bind(filmsNew[i])();
+    const filmCover = film.getImg.bind(filmsNew[i])();
+    const tmp = tmpMovieGrid.content.cloneNode(true);
+    tmp.getElementById('tmp-img').src = filmCover;
+    tmp.getElementById('tmp-name').innerText = filmName;
+    tmp.getElementById('tmp-description').innerText = filmDescription;
+    tmp.getElementById('fb').href = filmFb;
+    tmp.getElementById('tw').href = filmTwitter;
+    tmp.getElementById('bh').href = filmBehance;
+
+    movieGrid.appendChild(tmp);
+}
+
+
+
+
+
+
+
--- js/getPresent.js
+++ js/getPresent.js
@@ -1,10 +1,45 @@
-// let name_guest = document.getElementById('name_guest')
-// let button_send_info = document.getElementById('sendPresentInfo')
 
-// console.log('До нажатия кнопки отправить', name_guest.value)
+let buttonShowModalPresent = document.getElementById('getPresentModal');
+let modalPresentWindow = document.getElementById('modalPresent');
+let closePresentWindow = document.getElementById('closeModalPresent');
 
-// button_send_info.onclick = function(){
-//     let name = name_guest.value
-//     console.log('Имя ', name)
-// }
+buttonShowModalPresent.onclick = function(){
+    modalPresentWindow.style.display = 'block';
+}
 
+closePresentWindow.onclick = function () {
+    modalPresentWindow.style.display = 'none';
+}
+
+sendPresentInfo.onclick = function () {
+    let nameGuest = document.getElementById('name_guest').value;
+    let selectGuest = document.getElementById('select_guest').value;
+
+    if(nameGuest.value){
+        console.log('nameGuest', nameGuest.value); 
+        nameGuest.style.border = 'none'
+    } else {
+        nameGuest.style.border = '2px solid red'
+    }
+
+    if(selectGuest != 0){
+        console.log('selectGuest', selectGuest.value);
+        selectGuest.style.border = '1px solid #bebebe'
+    } else {
+        selectGuest.style.border = '2px solid red'
+    }
+
+    if (selectGuest.value != 0 && nameGuest.value){
+    
+    }
+}
+
+let nameGuest = document.getElementById('name_guest')
+nameGuest.change = function () {
+    console.log('До нажатия кнопки отправить nameGuest', nameGuest.value)
+}
+
+let selectGuest = document.getElementById('select_guest')
+selectGuest.change = function () {
+    console.log('До нажатия кнопки отправить selectGuest', selectGuest.value)
+}
